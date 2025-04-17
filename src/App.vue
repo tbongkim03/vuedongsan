8e55279cdbdf76600ae6f62c4881f961eeef5cc7
tbongkim03 <tbongkim03@gmial.com>
2025-04-17 10:46:40 +0900

    4 commit

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,41 @@
 <template>
-  
+
+  <!-- 오늘의 5분 숙제 : 
+
+  모달창 닫기버튼과 기능을 만들어오십시오.  -->
+  <div class="black-bg" v-if="모달창열렸니 == true">
+    <div class="white-bg">
+      <h4>상세페이지임</h4>
+      <h4>상세페이지 내용임</h4>
+      <span @click="모달창열렸니=false" style="align-self: flex-end;">닫기</span>
+    </div>
+    
+  </div>
+
+
+
+
+
+
   <div class="menu">
     <a v-for="작명 in 메뉴들" :key="작명">{{ 작명 }}</a>
     <!-- <a href="">Home</a>
     <a href="">Products</a>
     <a href="">About</a> -->
   </div>
+
+
+
+
+
   <!-- 오늘의 5분 숙제 : 
 
   v-for 문법을 이용해 지금 있는 상품목록을 반복문으로 축약해보십시오.
 
   가격은 신경쓰지말고 상품제목만 잘 보이면 됩니다. -->
   <div v-for="(value, i) in products" :key="value">
-    <h4>{{ value }}</h4>
+    <img :src="`src/assets/room${i}.jpg`" :alt="i" class="room-img">
+    <h4 @click="모달창열렸니 = true">{{ value }}</h4>
     <p>50 만원</p>
     <!-- 오늘의 5분 숙제 : 
 
@@ -25,7 +48,7 @@
 
 <script>
 
-import { isInDestructureAssignment } from 'vue/compiler-sfc';
+// import { isInDestructureAssignment } from 'vue/compiler-sfc';
 
  
 
@@ -44,6 +67,7 @@ export default {
       products : ['역삼동원룸', '천호동원룸', '마포구원룸'],
       메뉴들 : ['Home', 'Shop', 'About'],
       신고수 : [0, 0, 0],
+      모달창열렸니 : false,
     }
   },
   methods: {
@@ -74,4 +98,28 @@ export default {
   padding: 10px;
 }
 
+.room-img {
+  width: 100%;
+  margin-top: 40px;
+}
+
+/* 모달창 css */
+body {
+  margin: 0;
+}
+div {
+  box-sizing: border-box;
+}
+.black-bg {
+  width: 100%; height: 100%;
+  background: rgba(0, 0, 0, 0.5);
+  position: fixed; padding: 20px;
+}
+.white-bg {
+  width: 100%; background: white;
+  border-radius: 8px;
+  padding: 20px;
+  display: flex;
+  flex-direction: column;
+}
 </style> 
